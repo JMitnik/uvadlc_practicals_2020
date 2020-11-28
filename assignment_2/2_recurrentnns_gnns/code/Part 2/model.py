bbdb378115b4788e6c066d3239c4de10a66d40ea
Jonathan <JMitnik@gmail.com>
2020-11-28 15:21:36 +0100

    Initial training

--- assignment_2/2_recurrentnns_gnns/code/Part 2/model.py
+++ assignment_2/2_recurrentnns_gnns/code/Part 2/model.py
@@ -26,8 +26,25 @@ class TextGenerationModel(nn.Module):
                  lstm_num_hidden=256, lstm_num_layers=2, device='cuda:0'):
 
         super(TextGenerationModel, self).__init__()
-        # Initialization here...
+
+        self.embedding = nn.Embedding(vocabulary_size, lstm_num_hidden)
+
+        self.batch_size = batch_size
+        self.seq_length = seq_length
+        self.vocabulary_size = vocabulary_size
+        self.lstm_num_hidden = lstm_num_hidden
+        self.lstm_num_layer = lstm_num_layers
+        self.device = device
+        
+        self.lstm = nn.LSTM(
+            input_size=lstm_num_hidden, 
+            hidden_size=lstm_num_hidden, 
+            num_layers=lstm_num_layers, 
+            batch_first=True
+        ).to(device)
 
     def forward(self, x):
-        # Implementation here...
-        pass
+        x = self.embedding(x)
+        out, _ = self.lstm(x)
+
+        return out
