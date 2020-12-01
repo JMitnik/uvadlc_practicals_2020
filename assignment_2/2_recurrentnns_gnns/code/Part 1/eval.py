cef63e32837705ab288821b288bf5d8e8c4705f5
Jonathan <jmitnik@gmail.com>
2020-12-01 01:58:51 +0100

    Better plots

--- assignment_2/2_recurrentnns_gnns/code/Part 1/eval.py
+++ assignment_2/2_recurrentnns_gnns/code/Part 1/eval.py
@@ -19,20 +19,24 @@ def merge_csvs(base_path, csv_glob='*.csv'):
 all_accs_df = merge_csvs('results', 'accs.csv')
 all_loss_df = merge_csvs('results', 'losses.csv')
 
-def plot_metric_for_model(df, model_name, metric, color, title, figname):
+def plot_metric_for_model(df, model_name, metric, colors, title, figname):
     df = df.query(f'model == "{model_name}"')
-    metric_means = df.groupby('step').mean()[metric]
-    metric_std = df.groupby('step').std()[metric]
 
-    # Plotting starts
-    plt.fill_between(range(len(metric_means)), metric_means - metric_std, metric_means + metric_std, alpha=0.5, color=color)
-    plt.plot(range(len(metric_means)), metric_means, color=color)
+    seq_lengths = df.groupby('seq_length')
+
+    for idx, (key, df_seq_length) in enumerate(seq_lengths):
+        metric_means = df_seq_length.groupby('step').mean()[metric]
+        metric_std = df_seq_length.groupby('step').std()[metric]
+        # Plotting starts
+        plt.fill_between(range(len(metric_means)), metric_means - metric_std,  metric_means + metric_std, color=colors[idx], alpha=0.5)
+        plt.plot(range(len(metric_means)), metric_means, color=colors[idx], label=f"seq_length={key}")
+    
     plt.title(title)
+    plt.legend()
     plt.savefig(f'eval_results/{figname}')
-
     plt.cla()
 
-plot_metric_for_model(all_loss_df, 'LSTM', 'loss', 'r', 'General LSTM Losses across seeds 42, 1337, 1994 every 10 steps', 'LSTM-plot-loss')
-plot_metric_for_model(all_accs_df, 'LSTM', 'acc', 'g', 'General LSTM accuracies across seeds 42, 1337, 1994 every 10 steps', 'LSTM-plot-accs')
-plot_metric_for_model(all_loss_df, 'peepLSTM', 'loss', 'r', 'General peepLSTM losses across seeds 42, 1337, 1994 every 10 steps', 'peepLSTM-plot-loss')
-plot_metric_for_model(all_accs_df, 'peepLSTM', 'acc', 'g', 'General peepLSTM accuracies across seeds 42, 1337, 1994 every 10 steps', 'peepLSTM-plot-accs')
+plot_metric_for_model(all_loss_df, 'LSTM', 'loss', ['#FCD34D', '#B91C1C'], 'General LSTM Losses across seeds 42, 1337, 1994 every 10 steps', 'LSTM-plot-loss')
+plot_metric_for_model(all_accs_df, 'LSTM', 'acc', ['#6EE7B7', '#2563EB'], 'General LSTM accuracies across seeds 42, 1337, 1994 every 10 steps', 'LSTM-plot-accs')
+plot_metric_for_model(all_loss_df, 'peepLSTM', 'loss', ['#FCD34D', '#B91C1C'], 'General peepLSTM losses across seeds 42, 1337, 1994 every 10 steps', 'peepLSTM-plot-loss')
+plot_metric_for_model(all_accs_df, 'peepLSTM', 'acc', ['#6EE7B7', '#2563EB'], 'General peepLSTM accuracies across seeds 42, 1337, 1994 every 10 steps', 'peepLSTM-plot-accs')
